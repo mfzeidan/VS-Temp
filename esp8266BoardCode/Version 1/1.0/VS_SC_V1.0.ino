52d4121fce04c03d8e12fa0b0012c21ff6c1f879
Mark Zeidan <mfzeidan@gmail.com>
2017-01-25 18:18:58 -0500

    Update VS_SC_V1.0.ino

--- esp8266BoardCode/Version 1/1.0/VS_SC_V1.0.ino
+++ esp8266BoardCode/Version 1/1.0/VS_SC_V1.0.ino
@@ -18,7 +18,13 @@
 Ticker sleepTicker;
 
 
-
+// THIS IS WHERE THINGS NEED TO BE DEFINED
+//***************************************************************
+#define topic1 "esp_temp_test2"
+#define mqtt_client_name "201" //dont change
+//set the temp wire input here
+#define MAX_LOOP_TIME_MS     15000
+  
 
 
 /*
@@ -37,17 +43,24 @@ Ticker sleepTicker;
  * note you must also change the wifi at the bottom to edit the deviceID into it
  * TODO fix comment above
  * 
+ * 
+ * 
+ * LIFECYCLE OF APPLICATION
+ * 
+ * STARTUP - LED GOES ORANGE
+ * IF CONNECTS TO WIFI, BLINK GREEN
+ * if doesnt connect, goes red
+ * if loses connection to mqtt, blinks red
+ * if timeout, goes red
  */
 
+ 
 
 
-// THIS IS WHERE THINGS NEED TO BE DEFINED
-//***************************************************************
-#define topic1 "esp_temp_test2"
-#define mqtt_client_name "201" //dont change
-//set the temp wire input here
-#define MAX_LOOP_TIME_MS     15000
-  
+
+
+
+
 
 ///the following pins work on the ESP8266 ESP-12 to read the temperature
 //  14
@@ -55,7 +68,6 @@ Ticker sleepTicker;
 // 13 
 //PLEASE NOTE THAT THE PINS THAT DON'T WORK BELOW MAY BE DUE TO A WIRING FLAW ... but i doubt it :)
 // 12 DOES NOT WORK
-//note one of these pins will need to be used for the 3rd lead of the LED
 
  #define ONE_WIRE_BUS 13
 OneWire oneWire(ONE_WIRE_BUS);
@@ -63,17 +75,13 @@ DallasTemperature sensors(&oneWire);
 WiFiClient espClient;
 PubSubClient client(espClient);
 int deviceID = 201;
+String WiFiDevID = "test";
 
 
 
-//DEFINE THESE
-/*
 int redPin = 5;
 int greenPin = 4;
 int bluePin = 14;
-*/
-
-
 
 int greenArr[]= {0,255,0};
 int redArr[] = {0,255,0};
@@ -85,10 +93,10 @@ int blackArr[] = {0,0,0};
 
 char current_temp[32];
 //this is in seconds
+
 int sleepTimeS = 30;
 //sleeptimeS_AP is for when the device doesn't connect to the network
 int sleepTimeS_AP = 60;
-//how long it takes for the wifi to drop
 int wifi_timeout = 90;
 
 unsigned long startTime;
@@ -162,12 +170,12 @@ void reconnect() {
         // Attempt to connect
         if (client.connect(mqtt_client_name)) { //* See //NOTE below
             Serial.println("connected");
-            blink_lights(whiteArr,blackArr,2,75);
+            blink_lights(greenArr,blackArr,2,75);
         } else {
             Serial.print("failed, rc=");
             Serial.print(client.state());
             
-             blink_lights(redArr,blackArr,6,125); 
+             blink_lights(redArr,blackArr,2,75); 
             Serial.println(" try again in 5 seconds");
             // Wait 5 seconds before retrying
             delay(5000);
@@ -183,8 +191,7 @@ void sleepyTime() {
   Serial.printf("Sleeping. Loop took %d ms\n", elapsed);
   if (elapsed >= MAX_LOOP_TIME_MS) {
   
-     blink_lights(redArr, greenArr,3,200);
-
+    setColor(redArr);
    ESP.deepSleep(sleepTimeS * 10000000,WAKE_RF_DEFAULT);  }
   // It can take a while for the ESP to actually go to sleep.
   // When   it wakes up we start again in setup().
@@ -199,6 +206,9 @@ void setup() {
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
+
+  //SET THIS TO ORANGE IN PRODUCTION
+  
   setColor(redArr);
 
   WiFiManager wifiManager;
@@ -232,20 +242,23 @@ void setup() {
   //here  "AutoConnectAP" with password "password"
   //and goes into a blocking loop awaiting configuration
   //if (!wifiManager.autoConnect("AutoConnectAP1234")) {
-    if (!wifiManager.autoConnect("Device201")) {
+    if (!wifiManager.autoConnect("test")) {
 
 
-          blink_lights(redArr,blackArr,6,125);
+    // blink_lights(redArr,blackArr,6,125);
     Serial.println("failed to connect, we should reset as see if it connects");
+    setColor(redArr);
     delay(1000);
       ESP.deepSleep(sleepTimeS_AP * 1000000,WAKE_RF_DEFAULT);
     delay(500);
   }
-    wifiManager.autoConnect("Device201");
+    wifiManager.autoConnect("test");
     //wifiManager.autoConnect("AutoConnectAP1234");
   //if you get here you have connected to the WiFi
   Serial.println("connected...yeey :)");
   //blink_lights(green_pin,6,200);
+
+//CONNECT TO WIFI GO GREEN
   
     blink_lights(greenArr,blackArr,3,50);
     delay(200);
@@ -275,7 +288,7 @@ void loop() {
 
        // on the first reading it spits out 185 degrees if it measures something, not sure why but just make it rerequest a value
 
-      while(temp_float > 184 && temp_float < 186){
+      while(temp_float > 184.5 && temp_float < 185.5){
         Serial.println("temp not set yet");
         sensors.requestTemperatures(); // Send the command to get temperatures
       temp_float = sensors.getTempFByIndex(0);
